da83d32e406566b66313e919d16288e4c525b89a
arick <arickahamed700@gmail.com>
2025-07-22 19:49:53 +0600

    preparing the backend for the frontend part

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-  origin: 'http://localhost:3000',  
+  origin: 'https://devs-nest-client-hbnq.vercel.app',  
   credentials: true                 
 }));
 
@@ -32,7 +32,7 @@ mongoose.connect(mongoUri, {
 app.use("/api/tasks", taskRoutes);
 
 app.listen(8080, () => {
-  console.log("Server running on http://localhost:8080");
+  console.log("Server running on https://devs-nest-client-hbnq.vercel.app");
 });
 
 export default app;
